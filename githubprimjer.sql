0edeaf3de7e4828ec8c704ade3a39f0e62b017ea
Filip Đunda <93283413+Fdjunda@users.noreply.github.com>
2021-11-17 18:41:13 +0100

    dodao charset i novi unos

--- githubprimjer.sql
+++ githubprimjer.sql
@@ -1,7 +1,10 @@
-drop databases if exists githubprimjer;
-create databases githubprimjer;
+drop database if exists githubprimjer;
+create database githubprimjer character set utf8;
 use githubprimjer;
 create table git(
      sifra int not null primary key auto_increment,
 naziv varchar(50) not null
 );
+
+insert into git(sifra,naziv) values (null,'test');
+
