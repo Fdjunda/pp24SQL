de43389e4c6d23cc39f6e5ef1b5a8a1ab63150d8
Filip Đunda <93283413+Fdjunda@users.noreply.github.com>
2021-11-17 18:38:26 +0100

    pokazujem kako se radi sa git-om

---
+++ githubprimjer.sql
@@ -0,0 +1,7 @@
+drop databases if exists githubprimjer;
+create databases githubprimjer;
+use githubprimjer;
+create table git(
+     sifra int not null primary key auto_increment,
+naziv varchar(50) not null
+);
