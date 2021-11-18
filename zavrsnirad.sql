e2a29fff4adc29cfbb7b31184ed486f2cf1dc463
Filip Đunda <93283413+Fdjunda@users.noreply.github.com>
2021-11-18 13:48:09 +0100

    Create zavrsnirad.sql

---
+++ zavrsnirad.sql
@@ -0,0 +1,20 @@
+drop database if exists zavrsnirad;
+create database zavrsnirad;
+#ime app pri kraju rada
+use zavrsnirad;
+
+create table kupac(
+    sifra int not null primary key auto_increment,
+    ime varchar(20) not null,
+    prezime varchar(20) not null)
+    artikl varchar(50)
+    cijena decimal (18,2);
+
+    create table dostavljac(
+        sifra int not null primary key auto_increment,
+        ime varchar(20),
+        preizme varchar(20),
+        lokacija varchar(50)
+        vrijeme dostave datetime
+    );
+    
