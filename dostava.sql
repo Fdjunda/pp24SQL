e86dd6ad8820387407433cd0e7f393e45560ade2
Filip Đunda <93283413+Fdjunda@users.noreply.github.com>
2021-11-22 21:24:23 +0100

    novi uopdate

--- dostava.sql
+++ dostava.sql
@@ -6,7 +6,7 @@ use zavrsnirad --default_character_set=utf8;
     create table kupci(
     id int not null primary key auto_increment,
     ime varchar(50) not null,
-    prezime varchar(50) not null,    
+    prezime varchar(50) not null, 
     email varchar(50),
     kontakt_broj varchar(30),
     adresa varchar(60) not null,
@@ -24,7 +24,8 @@ use zavrsnirad --default_character_set=utf8;
     create table narudzbe(
     id int not null primary key auto_increment,
     kupci_id int,
-    ukupan_iznos decimal(18,2),
+    trgovina varchar(100),
+    nacin_placanja varchar(50),
     placeno boolean);
 
     create table narudzbe_artikli(
@@ -39,6 +40,7 @@ use zavrsnirad --default_character_set=utf8;
     ALTER TABLE narudzbe ADD FOREIGN KEY (kupci_id) REFERENCES kupci(id);
     ALTER TABLE narudzbe_artikli ADD FOREIGN KEY (narudzba_id) REFERENCES narudzbe(id);
     ALTER TABLE narudzbe_artikli ADD FOREIGN KEY (artikli_id) REFERENCES artikli(id);
+   
     
     
 
@@ -55,9 +57,5 @@ use zavrsnirad --default_character_set=utf8;
     values('coca cola','gazirani napitak',12.99);
 
 
-    select * from narudzbe;
 
-    insert into narudzbe(ukupan_iznos,placeno)
-    values(12.99,1)
-
-
+    
