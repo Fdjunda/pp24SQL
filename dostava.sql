28c827aeccac4c413d2c1363f6ccdc03a99bd75f
Filip Đunda <93283413+Fdjunda@users.noreply.github.com>
2021-11-27 12:38:10 +0100

    'insert' kupci

--- dostava.sql
+++ dostava.sql
@@ -43,16 +43,27 @@ use zavrsnirad --default_character_set=utf8;
     
 
 
-    select * from artikli;
-
     insert into artikli(id,naziv,jedinicna_mjera,opis,cijena) values
     (null,'Joystick','kom','kontroler za ps4',399.99),
     (null,'Ps+','kom','pretplata za online igranje',438.00),
-    (null,'Playstation 4','kom','igrajuca konzola',2899.89),
+    (null,'Playstation 4','kom','igrajuča konzola',2899.89),
     (null,'Lan kabal','m','kabal za spajanje',2.89),
     (null,'VR head set','kom','vr ps4 virtualna stvarnost + 4 igrice',2892.00),
-    (null,'Coca cola','kom','bezalkoholno gazirano pice',12.99);
+    (null,'Coca cola','kom','bezalkoholno gazirano piće',12.99),
+    (null,'Jabuka zlatni delišes','kg','voće',8.99),
+    (null,'Mirisna svijeća','kom','razni mirisi i boje',25.00),
+    (null,'Mlijeko','kom','svježe mlijeko 1l',8.99);
 
+
+    insert into kupci(id,ime,prezime,email,kontakt_broj,adresa,pbroj,mjesto) values
+    (null,'Tomislav','Sabolić',null,null,'Osječka 28a', 31221,'Josipovac'),
+    (null,'Kristijan','Hegeduš',null,null,'Bizovačka 38',31221,'Josipovac'),
+    (null,'Matej','Papac',null,null,'Stradun 99', 20000,'Dubrovnik'),
+    (null,'Mladen','Božić',null,null,'Alojzja Stepinca 876', 53231,'Plitvička Jezera'),
+    (null,'Josip','Ervačić',null,null,'Ivana Gundulića 76', 31000,'Osijek'),
+    (null,'Grgo','Kvesić',null,null,'Vijenac Petrove Gore 3', 31000,'Osijek'),
+    (null,'Dušan','Strajinić',null,null,'Europske avenije 998', 10000,'Zagreb'),
+    (null,'Filip','Đunda',null,null,'Trh Hrvatskih branitelja 16', 31214,'Laslovo');
 
    
     
